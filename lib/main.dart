5dfc4012a6c0295660bd59bd079d09c607c26218
Ty <poehnell28@gmail.com>
2021-02-07 13:45:06 -0700

    created a map in the answeres to include points for the answers, created a getter called resultPhrase that returns a String resultText which will return different Strings depending on the score accumulated from the answers

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,46 @@ class _MyAppState extends State<MyApp> {
   final _questions = [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'Text': 'Black', 'score': 10},
+        {'Text': 'Yellow', 'score': 6},
+        {'Text': 'Green', 'score': 2},
+        {'Text': 'Blue', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Lizzard', 'Cow', 'Horse']
+      'answers': [
+        {'Text': 'Cow', 'score': 10},
+        {'Text': 'Chicken', 'score': 5},
+        {'Text': 'horse', 'score': 1},
+      ]
     },
     {
       'questionText': 'What\'s your favorite food?',
-      'answers': ['Pizza', 'Toast', 'KD']
+      'answers': [
+        {'Text': 'KD', 'score': 1},
+        {'Text': 'Pizza', 'score': 5},
+        {'Text': 'Butter Chicken', 'score': 10},
+      ]
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if(_questionIndex < _questions.length){
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -52,11 +66,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
-          answerQuestion: _answerQuestion,
-          questionIndex: _questionIndex,
-          questions: _questions,
-        )
-         : Result(),
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore),
       ),
     );
     // TODO: implement build
